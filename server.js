d5ab9373d68e752d3c906ad22cbbd85a9f20fad4
Jalen Greene <33211911+emerald-nomad@users.noreply.github.com>
2021-08-21 15:03:10 +0000

    addng colors module

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const dotenv = require("dotenv");
 const morgan = require("morgan");
+const colors = require("colors");
 const connectDB = require("./config/db");
 const { bootcamps } = require("./routes");
 
@@ -8,27 +9,35 @@ const { bootcamps } = require("./routes");
 dotenv.config({ path: "./config/config.env" });
 
 const main = async () => {
-  // Connect to DB
-  await connectDB();
+  let server;
 
-  const app = express();
+  try {
+    // Connect to DB
+    await connectDB();
 
-  // Dev loggin mididleware
-  if (process.env.NODE_ENV === "development") {
-    app.use(morgan("dev"));
+    const app = express();
+
+    // Dev loggin mididleware
+    if (process.env.NODE_ENV === "development") {
+      app.use(morgan("dev"));
+    }
+
+    // Routes
+    app.use("/api/v1/bootcamps", bootcamps);
+
+    const PORT = process.env.PORT || 5000;
+
+    server = app.listen(
+      PORT,
+      console.log(
+        `Server running in ${process.env.NODE_ENV} mode on port ${process.env.PORT}`
+          .yellow.bold
+      )
+    );
+  } catch (error) {
+    server.close();
+    console.error(`Error: ${error.message}`.red);
   }
-
-  // Routes
-  app.use("/api/v1/bootcamps", bootcamps);
-
-  const PORT = process.env.PORT || 5000;
-
-  app.listen(
-    PORT,
-    console.log(
-      `Server running in ${process.env.NODE_ENV} mode on port ${process.env.PORT}`
-    )
-  );
 };
 
 main();
