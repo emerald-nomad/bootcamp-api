a7df70d0ee718c5bcf82a76af96a2e77a2c9824c
Jalen Greene <33211911+emerald-nomad@users.noreply.github.com>
2021-08-23 22:02:12 +0000

    adding db seed file

---
+++ seed.js
@@ -0,0 +1,55 @@
+const fs = require("fs");
+const mongoose = require("mongoose");
+const colors = require("colors");
+const dotenv = require("dotenv");
+
+// Load env vars
+dotenv.config({ path: "./config/config.env" });
+
+// Load Models
+const { Bootcamp } = require("./models");
+
+// Connect to DB
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useFindAndModify: false,
+  useUnifiedTopology: true,
+});
+
+// Read JSON files
+const bootcamps = JSON.parse(
+  fs.readFileSync(`${__dirname}/_data/bootcamps.json`, "utf-8")
+);
+
+// Export to DB
+const exportData = async () => {
+  try {
+    await Bootcamp.create(bootcamps);
+
+    console.log("Data Exported...".green.inverse);
+  } catch (err) {
+    console.error(err);
+  } finally {
+    process.exit();
+  }
+};
+
+// Delete data
+const deleteData = async () => {
+  try {
+    await Bootcamp.deleteMany();
+
+    console.log("Data Destroyed...".red.inverse);
+  } catch (err) {
+    console.error(err);
+  } finally {
+    process.exit();
+  }
+};
+
+if (process.argv[2] === "-e") {
+  exportData();
+} else if (process.argv[2] === "-d") {
+  deleteData();
+}
